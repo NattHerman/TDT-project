e097acefaf64824d4c0ca330171fe7a5c4aeb3eb
NattHerman <daghermanoyerhamn@gmail.com>
2026-03-11 14:03:49 +0100

    declare HexagonalButton

---
+++ hexagonalbutton.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "board.h"
+namespace Hex {
+
+class HexagonalButton {
+    vec2<int> tile; // Which tile on the board this button corresponds to.
+    int radius = 25;
+    bool isSelected = false;
+
+public:
+    void draw();
+    vec2<int> getScreenPosition();
+
+    friend class GUI;
+};
+
+} // namespace Hex
