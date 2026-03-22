0d3143cca3f895130214a92e79458bea8c63f0a4
NattHerman <daghermanoyerhamn@gmail.com>
2026-03-22 14:25:32 +0100

    Create HexGrid class

---
+++ UI/HexGrid.cpp
@@ -0,0 +1,34 @@
+#include "HexGrid.h"
+#include <cmath>
+
+Hex::vec2<int> Hex::UI::HexGrid::calculateChildPosition(int index) {
+    std::shared_ptr<HexTile> child = getChild(index);
+
+    static constexpr double angle = 2.0*M_PI / 12.0;
+
+    static const vec2<double> xDirection{std::cos(angle), std::sin(angle)};
+    static const vec2<double> yDirection{std::cos(angle), -std::sin(angle)};
+
+    vec2<double> alignedPosition = (xDirection * child->getTile().x + yDirection * child->getTile().y) * child->getRadius()*2;
+
+    return static_cast<vec2<int>>(alignedPosition);
+}
+
+void Hex::UI::HexGrid::alignChildrenPositions() {
+    for (int i = 0; i < children.size(); ++i) {
+        std::shared_ptr<HexTile> child = getChild(i);
+
+        child->setPosition(calculateChildPosition(i));
+    }
+}
+
+void Hex::UI::HexGrid::addChild(const std::shared_ptr<HexTile> &child) {
+    // Cast to std::shared_ptr<UINode>
+    children.emplace_back(child);
+}
+
+void Hex::UI::HexGrid::update() {
+    alignChildrenPositions();
+    
+    updateChildren();
+}
---
+++ UI/HexGrid.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "UINode.h"
+#include "HexTile.h"
+
+namespace Hex {
+namespace UI {
+
+// Aligns children to a hexagonal grid, where all children are HexTile
+class HexGrid : public UINode {
+    vec2<int> calculateChildPosition(int index);
+    void alignChildrenPositions();
+
+public:
+    // Only children of type HexTile can be added.
+    void addChild(const std::shared_ptr<HexTile> &child);
+
+    std::shared_ptr<HexTile> getChild(int index) const { return std::static_pointer_cast<HexTile>(children.at(index)); }
+
+    void update() override;
+
+    HexGrid() = default;
+    HexGrid(std::string name): UINode{name} {}
+};
+
+} // namespace UI
+} // namespace Hex
