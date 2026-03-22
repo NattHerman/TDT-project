463fd5b0c1f5d5ea30c5b7e18dfb318f5d0ef861
NattHerman <daghermanoyerhamn@gmail.com>
2026-03-22 15:59:33 +0100

    rect<T> constructors and getCenter()

--- rect.h
+++ rect.h
@@ -9,6 +9,14 @@ template <typename T>
 struct rect {
     vec2<T> position{0, 0};
     vec2<T> size{0, 0};
+
+    vec2<T> getCenter() {
+        return position + size / 2;
+    }
+
+    rect() = default;
+    rect(vec2<T> size): size{size} {}
+    rect(vec2<T> position, vec2<T> size): position{position}, size{size} {}
 };
 
 } // namespace Hex
