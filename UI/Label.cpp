c9a69b2cf5e328df802518bc9558bb1aa254bf25
NattHerman <daghermanoyerhamn@gmail.com>
2026-03-26 14:11:47 +0100

    Create Label class

---
+++ UI/Label.cpp
@@ -0,0 +1,12 @@
+#include "Label.h"
+
+void Hex::UI::Label::draw() {
+    drawHexagon();
+
+    // Draw text
+    static const int fontHeight = 30;
+    vec2<int> topLeftText = getGlobalPosition() - vec2<int>{15, fontHeight/2 + 5};
+    windowPtr->draw_text(topLeftText, text, TDT4102::Color::white, fontHeight);
+
+    drawChildren();
+}
---
+++ UI/Label.h
@@ -0,0 +1,20 @@
+#include "UINode.h"
+#include "HexTile.h"
+
+namespace Hex {
+namespace UI {
+
+class Label : public HexTile {
+public:
+    std::string text;
+
+    void setColor(TDT4102::Color newColor) { color = newColor; }
+
+    void draw() override;    
+
+    Label(std::string text, int longness, std::shared_ptr<TDT4102::AnimationWindow> windowPtr)
+    : HexTile{{0, 0}, windowPtr, longness}, text{text} {}
+};
+
+} // namespace UI
+} // namespace Hex
