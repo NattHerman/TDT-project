8f2ddab0161d2c834d601a0130874c0a0b628ead
NattHerman <daghermanoyerhamn@gmail.com>
2026-03-29 14:04:27 +0200

    align label text using longness

--- UI/Label.cpp
+++ UI/Label.cpp
@@ -5,7 +5,7 @@ void Hex::UI::Label::draw() {
 
     // Draw text
     static const int fontHeight = 30;
-    vec2<int> topLeftText = getGlobalPosition() - vec2<int>{15, fontHeight/2 + 5};
+    vec2<int> topLeftText = getGlobalPosition() - vec2<int>{longness/2, fontHeight/2 + 5};
     windowPtr->draw_text(topLeftText, text, TDT4102::Color::white, fontHeight);
 
     drawChildren();
