134f8a6f7bb11fa0af6a9872d6bd6b7f3702ab82
NattHerman <daghermanoyerhamn@gmail.com>
2026-04-09 20:48:43 +0200

    calculateChildPosition -> calculateTilePosition, make more general

--- UI/HexGrid.cpp
+++ UI/HexGrid.cpp
@@ -1,15 +1,13 @@
 #include "HexGrid.h"
 #include <cmath>
 
-Hex::vec2<int> Hex::UI::HexGrid::calculateChildPosition(int index) {
-    std::shared_ptr<HexTile> child = getChild(index);
-
+Hex::vec2<int> Hex::UI::HexGrid::calculateTilePosition(Hex::vec2<int> tile, int radius) {
     static constexpr double angle = 2.0*M_PI / 12.0;
 
     static const vec2<double> xDirection{std::cos(angle), std::sin(angle)};
     static const vec2<double> yDirection{std::cos(angle), -std::sin(angle)};
 
-    vec2<double> alignedPosition = (xDirection * child->getTile().x + yDirection * child->getTile().y) * child->getRadius()*2;
+    vec2<double> alignedPosition = (xDirection * tile.x + yDirection * tile.y) * radius*2;
 
     return static_cast<vec2<int>>(alignedPosition);
 }
@@ -18,7 +16,7 @@ void Hex::UI::HexGrid::alignChildrenPositions() {
     for (int i = 0; i < children.size(); ++i) {
         std::shared_ptr<HexTile> child = getChild(i);
 
-        child->setPosition(calculateChildPosition(i));
+        child->setPosition(calculateTilePosition(child->getTile(), child->getRadius()));
     }
 }
 
--- UI/HexGrid.h
+++ UI/HexGrid.h
@@ -8,14 +8,14 @@ namespace UI {
 
 // Aligns children to a hexagonal grid, where all children are HexTile
 class HexGrid : public UINode {
-    vec2<int> calculateChildPosition(int index);
     void alignChildrenPositions();
 
 public:
     // Only children of type HexTile can be added.
     void addChild(const std::shared_ptr<HexTile> &child);
-
     std::shared_ptr<HexTile> getChild(int index) const { return std::static_pointer_cast<HexTile>(children.at(index)); }
+
+    vec2<int> calculateTilePosition(Hex::vec2<int> tile, int radius);
 
     void update() override;
 
