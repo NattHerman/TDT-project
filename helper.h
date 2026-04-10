acf319fba82ccf91be22527e4aa5066df13b8f8e
NattHerman <daghermanoyerhamn@gmail.com>
2026-04-10 14:23:07 +0200

    move functions out of main.cpp

---
+++ helper.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "testing.h"
+#include "helper.h"
+#include "UI/Button.h"
+
+void drawBoundingBoxes(std::shared_ptr<Hex::UI::UINode> node, std::shared_ptr<TDT4102::AnimationWindow> windowPtr);
+
+void populateButtonGrid(std::shared_ptr<Hex::UI::HexGrid> grid, std::shared_ptr<Hex::Game> game, std::shared_ptr<TDT4102::AnimationWindow> windowPtr);
+
+void newGame(std::shared_ptr<Hex::UI::HexGrid> grid, std::shared_ptr<Hex::Game> game, std::shared_ptr<TDT4102::AnimationWindow> windowPtr);
+
+void forward(std::shared_ptr<int> index, std::shared_ptr<Hex::Game> game);
+
+void backward(std::shared_ptr<int> index, std::shared_ptr<Hex::Game> game);
+
+void drawPoints(const std::vector<Hex::vec2<int>> &points, TDT4102::Color color, std::shared_ptr<TDT4102::AnimationWindow> windowPtr);
+
+std::vector<Hex::vec2<int>> tilesToPoints(const std::vector<Hex::vec2<int>> &tiles, std::shared_ptr<Hex::UI::HexGrid> grid);
